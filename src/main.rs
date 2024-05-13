1205e8489b9839303191cb40bbb1da38e1c46d1a
ryqdev <ykshang4119@gmail.com>
2024-05-14 00:48:59 +0800

    finish client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+pub mod demo;
+use demo::demo_interface_client::DemoInterfaceClient;
+use demo::PingRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = DemoInterfaceClient::connect("http://127.0.0.1:8080").await?;
+
+    let request = tonic::Request::new(PingRequest{
+        message: "hello".into(),
+    });
+    let response = client.ping(request).await?;
+    println!("response: {:?}", response);
+
+    Ok(())
 }
